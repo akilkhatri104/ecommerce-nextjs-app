c297cce48a563c42e3413a30dbf43fd312070a70
Akil Khatri <khatriakil104@gmail.com>
2025-06-29 10:12:02 +0530

    Add ESLint configuration to ignore errors during production builds

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [new URL("https://dummyimage.com/**")],
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
